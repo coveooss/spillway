d83d53cb871d1dff40af2efd8eb21ad83c1f9140
gsimard <gsimard@coveo.com>
2016-05-20 09:09:00 -0400

    Applied @msanfacon's review

--- src/main/java/com/coveo/spillway/memory/InMemoryStorage.java
+++ src/main/java/com/coveo/spillway/memory/InMemoryStorage.java
@@ -56,6 +56,7 @@ public class InMemoryStorage implements LimitUsageStorage {
         mapWithThisExpiration.put(
             override.getLimitKey(), new AtomicInteger(override.getNewValue()));
       }
+      removeExpiredEntries();
     }
   }
 
