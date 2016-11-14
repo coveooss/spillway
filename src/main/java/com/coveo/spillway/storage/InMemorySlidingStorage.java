e3b3c4a56bc159eb7cca9a31f30167e9a45f376f
Emile Fugulin <emilefugulin@hotmail.com>
2016-11-14 10:58:04 -0500

    Fix formatting

--- src/main/java/com/coveo/spillway/storage/InMemorySlidingStorage.java
+++ src/main/java/com/coveo/spillway/storage/InMemorySlidingStorage.java
@@ -24,12 +24,11 @@ import com.coveo.spillway.storage.utils.Capacity;
  * The difference with {@link InMemoryStorage} is that instead of fixed time
  * buckets, this implementation uses a sliding window of time and counts
  * the number of requests made in that window.
- * 
+ *
  * @author Emile Fugulin
  * @since 1.1.0
  */
-public class InMemorySlidingStorage implements LimitUsageStorage
-{
+public class InMemorySlidingStorage implements LimitUsageStorage {
   private Map<Instant, Map<LimitKey, Capacity>> map = new ConcurrentHashMap<>();
   private Clock clock = Clock.systemDefaultZone();
   private Duration retention;
@@ -37,31 +36,31 @@ public class InMemorySlidingStorage implements LimitUsageStorage
 
   /**
    * Constructor for the {@link InMemorySlidingStorage}.
-   * 
+   *
    * @param retention The retention duration should be greater than highest limit duration, but
    *                  beware that the higher the retention is, the higher the memory usage will be.
    * @param slideSize The slide size determines the precision of the count, but beware that a smaller
    *                  slide size means more memory and more network traffic to sync the limits if needed.
    */
-  public InMemorySlidingStorage(Duration retention,
-                                Duration slideSize)
-  {
+  public InMemorySlidingStorage(Duration retention, Duration slideSize) {
     this.retention = retention;
     this.slideSize = slideSize;
   }
 
   @Override
-  public Map<LimitKey, Integer> addAndGet(Collection<AddAndGetRequest> requests)
-  {
+  public Map<LimitKey, Integer> addAndGet(Collection<AddAndGetRequest> requests) {
     Map<LimitKey, Integer> updatedEntries = new LinkedHashMap<>();
 
     for (AddAndGetRequest request : requests) {
-      Instant bucket = Instant.ofEpochMilli((request.getEventTimestamp().toEpochMilli() / slideSize.toMillis())
-          * slideSize.toMillis());
+      Instant bucket =
+          Instant.ofEpochMilli(
+              (request.getEventTimestamp().toEpochMilli() / slideSize.toMillis())
+                  * slideSize.toMillis());
 
       LimitKey limitKey = LimitKey.fromRequest(request);
 
-      Map<LimitKey, Capacity> mapWithThisExpiration = map.computeIfAbsent(bucket, (key) -> new HashMap<>());
+      Map<LimitKey, Capacity> mapWithThisExpiration =
+          map.computeIfAbsent(bucket, (key) -> new HashMap<>());
       Capacity counter = mapWithThisExpiration.computeIfAbsent(limitKey, (key) -> new Capacity());
       counter.addAndGet(request.getCost());
 
@@ -73,8 +72,7 @@ public class InMemorySlidingStorage implements LimitUsageStorage
   }
 
   @Override
-  public Map<LimitKey, Integer> debugCurrentLimitCounters()
-  {
+  public Map<LimitKey, Integer> debugCurrentLimitCounters() {
     removeExpiredEntries();
     return map.values()
         .stream()
@@ -83,42 +81,36 @@ public class InMemorySlidingStorage implements LimitUsageStorage
   }
 
   @Override
-  public void close() throws Exception
-  {
-  }
+  public void close() throws Exception {}
 
-  private void removeExpiredEntries()
-  {
+  private void removeExpiredEntries() {
     Instant oldest = Instant.now(clock).minus(retention);
-    Set<Instant> expiredEntries = map.keySet()
-                                     .stream()
-                                     .filter(bucket -> bucket.isBefore(oldest))
-                                     .collect(Collectors.toSet());
+    Set<Instant> expiredEntries =
+        map.keySet().stream().filter(bucket -> bucket.isBefore(oldest)).collect(Collectors.toSet());
     map.keySet().removeAll(expiredEntries);
   }
 
-  private Integer calculateLimitTotal(Instant currentBucket,
-                                      Duration limitSize,
-                                      LimitKey limitKey)
-  {
+  private Integer calculateLimitTotal(
+      Instant currentBucket, Duration limitSize, LimitKey limitKey) {
     Instant oldestBucket = currentBucket.minus(limitSize);
 
     return map.entrySet()
-              .stream()
-              .filter(entry -> entry.getKey().compareTo(oldestBucket) > 0)
-              .flatMap(entry -> entry.getValue().entrySet().stream())
-              .filter(entry -> limitKeysEquals(entry.getKey(), limitKey))
-              .peek(entry2 -> System.out.println(entry2.getKey().toString() + " : " + entry2.getValue().get()))
-              .map(entry -> entry.getValue().get())
-              .mapToInt(Integer::intValue)
-              .sum();
+        .stream()
+        .filter(entry -> entry.getKey().compareTo(oldestBucket) > 0)
+        .flatMap(entry -> entry.getValue().entrySet().stream())
+        .filter(entry -> limitKeysEquals(entry.getKey(), limitKey))
+        .peek(
+            entry2
+                -> System.out.println(entry2.getKey().toString() + " : " + entry2.getValue().get()))
+        .map(entry -> entry.getValue().get())
+        .mapToInt(Integer::intValue)
+        .sum();
   }
 
   // Necessary since the equals method of the LimitKey checks the bucket and we don't care about it
-  private boolean limitKeysEquals(LimitKey first,
-                                 LimitKey second)
-  {
-    return first.getResource() == second.getResource() && first.getLimitName() == second.getLimitName()
+  private boolean limitKeysEquals(LimitKey first, LimitKey second) {
+    return first.getResource() == second.getResource()
+        && first.getLimitName() == second.getLimitName()
         && first.getProperty() == second.getProperty();
   }
 }
--- src/test/java/com/coveo/spillway/storage/InMemorySlidingStorageTest.java
+++ src/test/java/com/coveo/spillway/storage/InMemorySlidingStorageTest.java
@@ -15,31 +15,31 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class InMemorySlidingStorageTest
-{
+public class InMemorySlidingStorageTest {
   private static final String RESOURCE1 = "someResource";
   private static final String LIMIT1 = "someLimit";
   private static final String PROPERTY1 = "someProperty";
   private static final Duration EXPIRATION = Duration.ofMinutes(1);
   private static final Duration SLIDE_SIZE = Duration.ofSeconds(1);
   private static final Instant TIMESTAMP = Instant.now();
-  
+
   @Mock private Clock clock;
 
-  @InjectMocks private InMemorySlidingStorage storage = new InMemorySlidingStorage(EXPIRATION, SLIDE_SIZE);
+  @InjectMocks
+  private InMemorySlidingStorage storage = new InMemorySlidingStorage(EXPIRATION, SLIDE_SIZE);
 
   @Before
   public void setup() {
     when(clock.instant()).thenReturn(Instant.now());
   }
-  
+
   @Test
   public void canAddValues() {
     int result =
         storage.addAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP, 5).getValue();
     assertThat(result).isEqualTo(5);
   }
-  
+
   @Test
   public void canAddValuesToExisitingCounters() {
     storage.incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP);
@@ -48,25 +48,38 @@ public class InMemorySlidingStorageTest
 
     assertThat(result).isEqualTo(6);
   }
-  
+
   @Test
   public void canAddValuesInAnotherSlice() {
     storage.incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP);
     int result =
-        storage.addAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP.plus(SLIDE_SIZE.multipliedBy(2)), 5).getValue();
+        storage
+            .addAndGet(
+                RESOURCE1,
+                LIMIT1,
+                PROPERTY1,
+                EXPIRATION,
+                TIMESTAMP.plus(SLIDE_SIZE.multipliedBy(2)),
+                5)
+            .getValue();
 
     assertThat(result).isEqualTo(6);
   }
-  
+
   @Test
   public void oldValuesAreRemoved() {
     storage.incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP).getValue();
-    storage.incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP.plus(EXPIRATION.dividedBy(2))).getValue();
-    
+    storage
+        .incrementAndGet(
+            RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP.plus(EXPIRATION.dividedBy(2)))
+        .getValue();
+
     when(clock.instant()).thenReturn(TIMESTAMP.plus(EXPIRATION));
-    
-    int result = storage.incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP.plus(EXPIRATION)).getValue();
+
+    int result =
+        storage
+            .incrementAndGet(RESOURCE1, LIMIT1, PROPERTY1, EXPIRATION, TIMESTAMP.plus(EXPIRATION))
+            .getValue();
     assertThat(result).isEqualTo(2);
   }
-
 }
