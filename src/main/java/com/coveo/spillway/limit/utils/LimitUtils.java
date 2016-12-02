6618fe5b8fbf2ee8f2b5c66f370fa919ead6e6e2
Emile Fugulin <emilefugulin@hotmail.com>
2016-12-02 12:57:19 -0500

    Fix formatting

--- src/main/java/com/coveo/spillway/limit/utils/LimitUtils.java
+++ src/main/java/com/coveo/spillway/limit/utils/LimitUtils.java
@@ -3,8 +3,7 @@ package com.coveo.spillway.limit.utils;
 import java.time.Duration;
 import java.time.Instant;
 
-public class LimitUtils
-{
+public class LimitUtils {
   public static Instant calculateBucket(Instant timestamp, Duration limitDuration) {
     return Instant.ofEpochMilli(
         (timestamp.toEpochMilli() / limitDuration.toMillis()) * limitDuration.toMillis());
