f3f2939b947bb3b9a19003184e99ffdfca9062fc
Prasad Perera <pperera@coveo.com>
2020-02-26 15:17:20 -0500

    Improve exception naming.

--- src/main/java/com/coveo/spillway/storage/utils/RedisCounterLuaScriptLoader.java
+++ src/main/java/com/coveo/spillway/storage/utils/RedisCounterLuaScriptLoader.java
@@ -1,6 +1,6 @@
 package com.coveo.spillway.storage.utils;
 
-import com.coveo.spillway.exception.SpillwayLuaResourceErrorException;
+import com.coveo.spillway.exception.SpillwayLuaResourceException;
 import com.coveo.spillway.storage.RedisStorage;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,13 +10,13 @@ public class RedisCounterLuaScriptLoader {
   private static final String COUNTER_LUA_SCRIPT_PATH =
       "com/coveo/spillway/storage/redisCounter.lua";
 
-  public static String getCounterLuaScript() throws SpillwayLuaResourceErrorException {
+  public static String getCounterLuaScript() throws SpillwayLuaResourceException {
     try {
       return IOUtils.toString(
           RedisStorage.class.getClassLoader().getResourceAsStream(COUNTER_LUA_SCRIPT_PATH),
           StandardCharsets.UTF_8);
     } catch (IOException e) {
-      throw new SpillwayLuaResourceErrorException(COUNTER_LUA_SCRIPT_PATH, e);
+      throw new SpillwayLuaResourceException(COUNTER_LUA_SCRIPT_PATH, e);
     }
   }
 }
